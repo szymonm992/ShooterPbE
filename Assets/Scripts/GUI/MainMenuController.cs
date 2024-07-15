6de9c9658a91ae6a8d4eb36fd8328e68ad18729d
Black heart <szymon.m992@gmail.com>
2024-07-15 02:46:16 +0200

    Minor logic update

--- Assets/Scripts/GUI/MainMenuController.cs
+++ Assets/Scripts/GUI/MainMenuController.cs
@@ -1,5 +1,7 @@
 using Elympics;
 using JetBrains.Annotations;
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
@@ -12,31 +14,7 @@ namespace ShooterPbE.GUI
 
         [SerializeField] private Button startGameButton;
         [SerializeField] private CanvasGroup loadingScreenGroup;
-
-        private void Awake()
-        {
-
-            DebugManager.instance.enableRuntimeUI = false;
-        }
-
-        private void Start()
-        {
-            startGameButton.onClick.AddListener(StartButtonClicked);
-            TogglePlayButton(ElympicsLobbyClient.Instance.IsAuthenticated);
-            ElympicsLobbyClient.Instance.AuthenticationSucceeded += (_) => TogglePlayButton(true);
-        }
-
-        private void OnDestroy()
-        {
-            startGameButton.onClick.RemoveAllListeners();
-        }
-
-        private void StartButtonClicked()
-        {
-            TogglePlayButton(false);
-            ToggleLoadingScreen(true);
-            PlayOnline();
-        }
+        [SerializeField] private TextMeshProUGUI errorLabel;
 
         [UsedImplicitly]
         public async void PlayOnline()
@@ -45,6 +23,50 @@ namespace ShooterPbE.GUI
             ElympicsLobbyClient.Instance.PlayOnlineInRegion(region, null, null, MATCHMAKING_QUEUE);
 
             Debug.Log($"Joined matchmaking in region: {region} with ping {latency}");
+        }
+
+        private void Awake()
+        {
+            DebugManager.instance.enableRuntimeUI = false;
+        }
+
+        private void Start()
+        {
+            if (ElympicsLobbyClient.Instance == null)
+            {
+                return;
+            }
+
+            startGameButton.onClick.AddListener(StartButtonClicked);
+            TogglePlayButton(ElympicsLobbyClient.Instance.IsAuthenticated);
+            ElympicsLobbyClient.Instance.AuthenticationSucceeded += (_) => TogglePlayButton(true);
+            ElympicsLobbyClient.Instance.Matchmaker.MatchmakingFailed += OnMatchmakingFailed;
+        }
+
+        private void OnDestroy()
+        {
+            if (ElympicsLobbyClient.Instance == null)
+            {
+                return;
+            }
+
+            ElympicsLobbyClient.Instance.Matchmaker.MatchmakingFailed -= OnMatchmakingFailed;
+            startGameButton.onClick.RemoveAllListeners();
+        }
+
+        private void OnMatchmakingFailed((string error, Guid _) result) => SetErrorTextCaption(result.error);
+
+        private void StartButtonClicked()
+        {
+            SetErrorTextCaption(string.Empty);
+            TogglePlayButton(false);
+            ToggleLoadingScreen(true);
+            PlayOnline();
+        }
+
+        private void SetErrorTextCaption(string caption)
+        {
+            errorLabel.text = caption;
         }
 
         private void TogglePlayButton(bool value)
